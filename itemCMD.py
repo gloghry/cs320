d843ca21ec04f708cf7e7212c3e3e979a1de4e06
electrostar121 <electrostar121@gmail.com>
2022-04-24 22:55:56 -0700

    getting the cmd interface started

--- itemCMD.py
+++ itemCMD.py
@@ -1,14 +1,32 @@
 from itemDB import *
+import re
+
+def commandParser(commandInput):
+    commandParsed = ["", ""]
+
+    try:
+        commandParsed[1] = str(re.search(r"\(([A-Za-z0-9_]+)\)", commandInput))
+
+        try:
+            commandParsed[0] = commandInput.replace("({commandArgs})", "")
+            #print("3", command)
+        except:
+            print("Couldn't figure out what you were saying")
+    except:
+        print("Couldn't figure out what you were saying")   
+
+    return commandParsed
+
 
 if __name__ == '__main__':
-    items = itemDB()
-    items.updateList()
-    print(items.deleteItem("Longbow"))
-    print(items.addItem("Longbow", homebrew = True))
-    print(items.addItem("Longbow", homebrew = True))
-    #print(items.editItem("Longbow", "homebrew", False))
-    #print(items.editItem("Longbow", "homebrew", False))
-    print(items.editItem("Longbow", "homebrewW", False))
-    print(items.printItem("Longbow"))
-    print(items.deleteItem("Longbow"))
-    print(items.printItem("Longbow"))
+    items = itemDB()#Start the database
+    items.updateList()#Must call this function, else you are fucked
+
+    print("Welcome to the item database command line interface.\nThis the command line version of the database.\nType 'help()' to get a list of commands.\nType 'exit()' to exit the CMD inteface.")
+
+    command = ""
+
+    while command != "exit()":
+        commandInput = input(">")
+        commandParsed = commandParser(commandInput)
+        command = commandParsed[0]

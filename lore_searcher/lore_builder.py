07f283362ae16f581fc31bceca774e8faed46628
pixelcutter <nwmusicview@gmail.com>
2022-04-25 17:14:42 -0700

    some refactoring

--- lore_searcher/lore_builder.py
+++ lore_searcher/lore_builder.py
@@ -1,15 +1,13 @@
-import os
-import json
+import classes.cmdFuncs as cmdf
+from os import system
 from classes.LoreSearcher import Searcher
 from classes.LorePage import Page
 from classes.LoreSection import Section
 from classes.LoreMaster import Master
 
 master = Master("drizzt")
-s1 = Section("inspiration")
-master.addSection(s1)
-s2 = Section("bruh")
-master.addSection(s2)
+master.addSection(Section("inspiration"))
+master.addSection(Section("bruh"))
 master.cClass = "skirmisher"
 master.cSex = "male"
 master.cOrigin = "underdark"
@@ -19,163 +17,37 @@ master.cBio = """Drizzt Do'Urden was a drow. He stood about 5 feet and 4 inc
 textBound = 80
 searcher = Searcher("data/index")
 
-def addSection(args):
-    if len(args) != 1:
-        print("Invalid number of arguments: <add_section, sectionName>")
-        return
-    newSection = Section(args[0])
-    if master.addSection(newSection) == False:
-        print(f"Section '{args[0]}' already exists, aborting...")
 
-def printPage(args):
-    if len(args) != 1:
-        return
-    page = searcher.searchID(args[0])
-    if page != None:
-        page.printFull()
-    else:
-        print(f"Sorry, no page with id: {args[0]} exists in index")
-
-def deleteSection(args):
-    if len(args) != 1:
-        print("Invalid number of arguments: <del_section, sectionName>")
-        return
-    if master.delSection(args[0]) == False:
-        print(f"No '{args[0]}' section found, aborting...")
-
-def listSection(args):
-    if len(args) != 1:
-        print("Invalid number of arguments: <list_section,sectionName>")
-    if master.listSection(args[0]) == False:
-        print(f"Section '{args[0]}' was not found")
-
-def search(args):
-    if len(args) < 1:
-        return
-    query = args[0]
-    results = searcher.search(query)
-    if results['results'] == None:
-        print(f"0 results found for '{results['query']}', did you mean '{results['corrected']}'?")
-    else:
-        for result in results['results']:
-            result.printSummary()
-
-def addPage(args):
-    if len(args) != 2:
-        print("Invalid number of arguments: <del_page,sectionName,pageID>")
-        return
-    sName = args[0]
-    if sName not in master.sectionDict:
-        print(f"No section titled '{sName}' found")
-        return
-
-    pageID = args[1]
-    page = searcher.searchID(pageID)
-    if page == None:
-        print(f"Sorry, a page with id:{pageID} could not be found")
-        return
-
-    master.sectionDict[sName].addPage(page)
-    
-def delPage(args):
-    if len(args) != 2:
-        print("Invalid number of arguments: <del_page,sectionName,pageID>")
-        return
-    sName = args[0]
-    if sName not in master.sectionDict:
-        print(f"No section titled '{sName}' found")
-        return
-    pageID = args[1]
-    if pageID not in master.sectionDict[sName].pageIDs:
-        print(f"'{sName}' does not have that page")
-        return
-
-    master.sectionDict[sName].delPage(pageID)
-
-def saveLore(args):
-    filename = master.cName.replace(' ', '_').lower()
-    path = f"lore_files/{filename}.lore"
-    if len(args) != 0:
-        print("Invalid save command: <save>")
-        return
-    if master.saveLore(path) == False:
-        print("An error occured while trying to save, aborting..")
-    else:
-        print("Lore saved successfully")
-
-def prettySave(args):
-    filename = master.cName.replace(' ', '_').lower()
-    path = f"lore_files/{filename}.txt"
-    if len(args) != 0:
-        print("Invalid pretty save command: <pretty_save>")
-        return
-    if master.prettySave(path, textBound) == False:
-        print("An error occured while trying to save, aborting..")
-    else:
-        print("Lore saved successfully")
-
-def loadLore(args):
-        if len(args) != 1:
-            print("Invalid number of arguments: <load_lore,path_to_file>")
-            return
-
-        filePath = args[0]
-        if not os.path.exists(filePath):
-            print(f"File path '{filePath}' could not be found, aborting")
-            return
-
-        with open(filePath, 'r') as fd:
-            data = json.load(fd)
-
-        master.cName = data['character-name']
-        master.cBio = data['cBio']
-        master.cClass = data['cClass']
-        master.cOrigin = data['cOrigin']
-        master.cRace = data['cRace']
-        master.cSex = data['cSex']
-        master.sectionNames = data['section-names']
-
-        for section in data['sections']:
-            newSection = Section(section['name'])
-            for page in section['page-list']:
-                tmpPage = searcher.searchID(page['id'])
-                if tmpPage == None:
-                    print(f"Sorry, a page with id:{page['id']} could not be found, skipping...")
-                    continue
-                newSection.addPage(tmpPage)
-            master.sectionDict[section['name']] = newSection
-
-        print("Lore loaded successfully")
-
-def printSection(args):
-    if len(args) != 1:
-        print("Invalid number of arguments: <print_section,sectionName>")
-        return
-    sName = args[0]
-    if sName not in master.sectionDict:
-        print(f"Section '{sName}' not found")
-        return
-    master.sectionDict[sName].printSection()
-
-def func(cmd, args):
+def cmdFuncs(cmd, args):
     funcs = {
-        "add_section": lambda: addSection(args),
-        "print_page": lambda: printPage(args),
-        "del_section": lambda: deleteSection(args),
-        "list": lambda: listSection(args),
-        "search": lambda: search(args),
-        "add_page": lambda: addPage(args),
-        "del_page": lambda: delPage(args),
-        "list_all": lambda: master.listAllSections(),
-        "save": lambda: saveLore(args),
-        "pretty_save": lambda: prettySave(args),
-        "load": lambda: loadLore(args),
+        "add_section": lambda: cmdf.addSection(master, args),
+        "as": lambda: cmdf.addSection(master, args),
+        "print_page": lambda: cmdf.printPage(searcher, args),
+        "pp": lambda: cmdf.printPage(searcher, args),
+        "del_section": lambda: cmdf.deleteSection(master, args),
+        "ds": lambda: cmdf.deleteSection(master, args),
+        "ls": lambda: cmdf.listSection(master, args),
+        "search": lambda: cmdf.search(searcher, args),
+        "s": lambda: cmdf.search(searcher, args),
+        "add_page": lambda: cmdf.addPage(master, searcher, args),
+        "ap": lambda: cmdf.addPage(master, searcher, args),
+        "del_page": lambda: cmdf.delPage(master, args),
+        "dp": lambda: cmdf.delPage(master, args),
+        "la": lambda: master.listAllSections(),
+        "save": lambda: cmdf.saveLore(master, args),
+        "pretty_save": lambda: cmdf.prettySave(master, textBound, args),
+        "psave": lambda: cmdf.prettySave(master, textBound, args),
+        "load": lambda: cmdf.loadLore(master, searcher, args),
         "print_lore": lambda: master.printLore(textBound),
-        "print_section": lambda: printSection(args)
+        "pl": lambda: master.printLore(textBound),
+        "print_section": lambda: cmdf.printSection(master, args),
+        "ps": lambda: cmdf.printSection(master, args),
+        "clear": lambda: system('clear')
     }
 
     if cmd not in funcs:
-        print(f"Command '{cmd}' not recognized. Enter 'help' to see available commands")
+        print(
+            f"Command '{cmd}' not recognized. Enter 'help' to see available commands")
         return
 
     funcs[cmd]()
@@ -185,8 +57,7 @@ while True:
     uInput = input("> ").lower()
     if uInput == 'q' or uInput == 'quit':
         break
-    if uInput =='':
+    if uInput == '':
         continue
     tmp = uInput.split(",")
-    func(tmp[0], list(map(lambda x: x.strip().lower(), tmp[1:])))
-
+    cmdFuncs(tmp[0], list(map(lambda x: x.strip().lower(), tmp[1:])))

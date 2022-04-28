871fbbfc10e35fc93ba9d91802835a3859111d75
electrostar121 <electrostar121@gmail.com>
2022-04-28 11:24:18 -0700

    working on the inventoryDB

--- inventoryDB.py
+++ inventoryDB.py
@@ -1,17 +1,70 @@
+import json
+import os
+
 class inventoryDB:
     def __init__(self):
         self.current = ""
+        self.inventoryList = []
+
+    def updateList(self):
+        self.inventoryList = []
+        for file in os.listdir("inventory/"):
+            if file.endswith(".inventory"):
+                self.inventoryList.append(self.inventoryName("inventory/" + file))
 
     def setInventory(self, name):
-        return self.jsonFormat(False, reason = "Cause yeah")
+        if(not self.inventoryExists(name)):
+            return self.jsonFormat(False, reason = "Inventory for {name} does not exist")
+
+        self.current = self.inventoryName(f"inventory/{self.fileName(name)}.inventory")
+
+        return self.jsonFormat(True)
 
     def createNewInventory(self, name):
-        return self.jsonFormat(False, reason = "Cause yeah")
+        if(self.inventoryExists(name)):
+            return self.jsonFormat(False, reason = "Inventory for {name} already exists")
+
+        with open(f"inventory/{self.fileName(name)}.inventory", "w") as newInventory:
+            json.dump({"name": name}, newInventory)
+
+        self.updateList()
+
+        return self.jsonFormat(True)
 
     def deleteInventory(self, name):
-        return self.jsonFormat(False, reason = "Cause yeah")
+        if(not self.inventoryExists(name)):
+            return self.jsonFormat(False, reason = "Inventory for {name} does not exist")
 
-    def incrementItem(self, itemName):
+        if os.path.exists(f'inventory/{self.fileName(name)}.inventory'):
+            os.remove(f'inventory/{self.fileName(name)}.inventory')
+        else:
+            return self.jsonFormat(False, reason = "No such file exists")
+
+        if(self.current = name):
+            self.current = ""
+
+        self.updateList()
+
+        return self.jsonFormat(True)
+
+    def incrementItem(self, itemName, amount):
+        if(not self.inventoryExists(name)):
+            return self.jsonFormat(False, reason = "Inventory for {name} does not exist")
+
+        inventory = {}
+
+        with open(f'inventory/{self.fileName(self.current)}.inventory', "r") as inventoryFile:
+            inventory = json.load(inventoryFile)
+            if itemName in inventory:
+                tmp = inventory[itemName]
+                tmp += amount
+                inventory[itemName] = tmp
+            else:
+                inventory.update({itemName, amount})
+            
+        with open(f'inventory/{self.fileName(self.current)}.inventory', "w") as inventoryFile:
+            json.dump(inventory, inventoryFile)
+        
         return self.jsonFormat(False, reason = "Cause yeah")
 
     def decrementItem(self, itemName):
@@ -19,6 +72,22 @@ class inventoryDB:
 
     def removeItem(self, itemName):
         return self.jsonFormat(False, reason = "Cause yeah")
+
+    def inventoryExists(self, name):
+        for inventoryName in self.itemList:
+            if (inventoryName == name):
+                return True
+
+        return False
+
+    def inventoryName(self, file):
+        file = open(filePath)
+        data = json.load(file)
+        file.close()
+        return data["name"] 
+
+    def fileName(self, name):#Converts an item's name to an acceptable file name
+        return name.lower().strip()  
 
     def jsonFormat(self, success, **kwargs):#Takes in the arguments and converts it into a dict/json
         dataFormated = {"success": success}
